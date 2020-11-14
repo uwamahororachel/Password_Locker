35d7b98e214dd19a94b915707315f1868c69fa1c
uwamahororachel <racheluwamahoro55@gmail.com>
2020-11-14 16:07:41 +0200

    appying final modifications for the credential file

--- credentials.py
+++ credentials.py
@@ -25,3 +25,37 @@ class Credentials:
     contact_list = [] # Empty contact list
  # Init method up here
     def save_credentials(self):
+
+        '''
+        save_credentials method saves credentials objects into credentials_list
+        '''
+
+        Credentials.credentials_list.append(self)
+      
+    def delete_credentials(self):
+
+        '''
+        delete_contact method deletes a save credentials from the credentials_list
+        '''
+
+        Credentials.credentials_list.remove(self)
+    @classmethod
+    def find_by_name(cls,name):
+        for credentials in cls.credentials_list:
+            if credentials.credentials_name == name:
+                return Credentials  
+    
+    @classmethod
+    def credentials_exist(cls,name):
+        '''
+        Method that checks if a credentials exists from the credentials list.
+        Args:
+            name: Acc name to search if it exists
+        Returns :
+            Boolean: True or false depending if the credentials exists
+        '''
+        for credentials in cls.credentials_list:
+            if credentials.password == name:
+                    return credentials
+
+        return False      
