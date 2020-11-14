ca1026e22921e024c98831893c1f7f7b35547cb5
uwamahororachel <racheluwamahoro55@gmail.com>
2020-11-14 16:14:28 +0200

    creat additional function

--- output.py
+++ output.py
@@ -4,3 +4,41 @@ from user import User
 import string
 import random
 # ...................................credentials...................................
+def create_credentials(pfacebook,pcanvas,ptwitter,pemail):
+    '''
+    Function to create a new contact
+    '''
+    new_credentials = Credentials(pfacebook,pcanvas,ptwitter,pemail)
+    return new_credentials
+
+def save_credentials(credentials):
+    '''
+    Function to save credentials
+    '''
+    credentials.save_credentials()
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
+def del_credentials(credentials):
+    '''
+    Function to delete a credentials
+    '''
+    credentials.delete_credentials()
+
+def check_existing_credentials(name):
+    '''
+    Function that check if an account exists with that name and return a Boolean
+    '''
+    return Credentials.credentials_exist(name)  
+def find_credentials(name):
+   '''
+   Function that finds a credential by username and returns the user credential
+   '''
+   return Credentials.find_by_name(name)
+
+def pw_gen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+# print(pw_gen(int(input('How many characters in your password?'))))
