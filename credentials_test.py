af80e6eeac18262ba9e087e59c4879b66d3a4cd3
uwamahororachel <racheluwamahoro55@gmail.com>
2020-11-14 16:11:15 +0200

    additional code for credential file

--- credentials_test.py
+++ credentials_test.py
@@ -18,3 +18,21 @@ class TestCredentials(unittest.TestCase):
         self.new_credentials = Credentials("tttttTTTTDWEW12","Uwamahoro@ws","eaeaeeatgg","TTTFGGEHJGHSRTMN")
  # create contact object
 
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credentials.facebook,"tttttTTTTDWEW12")
+        self.assertEqual(self.new_credentials.canvas,"Uwamahoro@ws")
+        self.assertEqual(self.new_credentials.twitter,"eaeaeeatgg")
+        self.assertEqual(self.new_credentials.email,"TTTFGGEHJGHSRTMN")
+
+    def test_save_credentials(self):
+        '''
+        test_save_contact test case to test if the credentials object is saved into
+         the contact list
+        '''
+        self.new_credentials.save_credentials() # saving the new contact
+        self.assertEqual(len(Credentials.credentials_list),1)
