6f3cfce0f0ae49ce3068de95e9c05ccc970cc5a0
uwamahororachel <racheluwamahoro55@gmail.com>
2020-11-14 16:13:52 +0200

    creating the output file

---
+++ output.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.6
+from credentials import Credentials
+from user import User
+import string
+import random
+# ...................................credentials...................................
